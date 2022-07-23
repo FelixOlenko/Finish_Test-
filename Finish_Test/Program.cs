3f4ad7f88ff2c12cfd38c1c9f64c0dffcd95c1f0
Felix Olenko <0feliks0@gmail.com>
2022-07-23 15:52:48 +0300

    3 Stage

--- Finish_Test/Program.cs
+++ Finish_Test/Program.cs
@@ -8,3 +8,27 @@ for (int i = 0; i < outputArray.Length; i++)
 {
     Console.Write($" \"{outputArray[i]}\"; ");
 }
+
+static string[] ReadArray()
+{
+    bool arrayEntered = false;
+    string[] words = new string[10];
+    for (int i = 0; !arrayEntered; i++)
+    {
+        if (i + 1 >= words.Length)
+        {
+            Array.Resize(ref words, (int)Math.Round(words.Length * 1.5));
+        }
+        string? arrayElement = Console.ReadLine();
+        if (String.IsNullOrEmpty(arrayElement))
+        {
+            Array.Resize(ref words, i);
+            arrayEntered = true;
+        }
+        else
+        {
+            words[i] = arrayElement;
+        }
+    }
+    return words;
+}
