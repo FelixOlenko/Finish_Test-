1c7e3b43bd30d6f20f3976f9137e0aa12f8c6612
Felix Olenko <0feliks0@gmail.com>
2022-07-23 15:51:54 +0300

    1 Stage

---
+++ Finish_Test/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+Console.WriteLine("Введите массив строк поочередно через Enter,\nвведите пустую строку, чтобы завершить ввод");
+var inputArray = ReadArray();
+var outputArray = RemoveBigEntries(inputArray);
+Console.WriteLine("Слова в которых не более 3-х символов:");
